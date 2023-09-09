59300ebebe619393856d470e6b7ccc2116f45314
459339321 <45264050+459339321@users.noreply.github.com>
2023-09-09 16:13:05 +0800

    Update sidebar.tsx
    标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -10,7 +10,6 @@ import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
 import MaskIcon from "../icons/mask.svg";
 import PluginIcon from "../icons/plugin.svg";
-import DragIcon from "../icons/drag.svg";
 
 import Locale from "../locales";
 
@@ -48,97 +47,97 @@ function useHotKey() {
     };
 
     window.addEventListener("keydown", onKeyDown);
-    返回 ()= >窗口。removeEventListener(“按键”，onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
   });
 }
 
-功能 使用DragSideBar() {
-  常数 限制 = (x:数字)= >数学。部(MAX_SIDEBAR_WIDTH，x);
+function useDragSideBar() {
+  const limit = (x: number) => Math.min(MAX_SIDEBAR_WIDTH, x);
 
-  常数 配置 = useAppConfig();
-  常数 startX = useRef(0);
-  常数 startDragWidth = useRef(配置。侧栏宽度 ?? 300);
-  常数 lastUpdateTime = useRef(约会。现在());
+  const config = useAppConfig();
+  const startX = useRef(0);
+  const startDragWidth = useRef(config.sidebarWidth ?? 300);
+  const lastUpdateTime = useRef(Date.now());
 
-  常数 handleMouseMove = useRef((e:MouseEvent) => {
-    如果 (约会。现在() < lastUpdateTime.目前的 + 50) {
-      返回;
+  const handleMouseMove = useRef((e: MouseEvent) => {
+    if (Date.now() < lastUpdateTime.current + 50) {
+      return;
     }
-lastUpdateTime。目前的=日期。现在();
-    常数 d= e。clientXstartX。目前的;
-    常数 nextWidth = 限制(startDragWidth。目前的+ d);
-配置。更新((配置) => (配置。侧栏宽度= nextWidth));
+    lastUpdateTime.current = Date.now();
+    const d = e.clientX - startX.current;
+    const nextWidth = limit(startDragWidth.current + d);
+    config.update((config) => (config.sidebarWidth = nextWidth));
   });
 
-  常数 handleMouseUp = useRef(() => {
-startDragWidth。目前的=配置。侧栏宽度 ?? 300;
-窗户。removeEventListener("鼠标移动"，handleMouseMove。目前的);
-窗户。removeEventListener("鼠标抬起"，handleMouseUp。目前的);
+  const handleMouseUp = useRef(() => {
+    startDragWidth.current = config.sidebarWidth ?? 300;
+    window.removeEventListener("mousemove", handleMouseMove.current);
+    window.removeEventListener("mouseup", handleMouseUp.current);
   });
 
-  常数 翁德拉穆斯敦 = (e:MouseEvent) => {
-startX。目前的= e。clientX;
+  const onDragMouseDown = (e: MouseEvent) => {
+    startX.current = e.clientX;
 
-窗户。addEventListener("鼠标移动"，handleMouseMove。目前的);
-窗户。addEventListener("鼠标抬起"，handleMouseUp。目前的);
+    window.addEventListener("mousemove", handleMouseMove.current);
+    window.addEventListener("mouseup", handleMouseUp.current);
   };
-  常数 isMobileScreen = useMobileScreen();
-  常数 应该缩小 =
-！isMobileScreen && config。侧栏宽度 < MIN_SIDEBAR_WIDTH;
+  const isMobileScreen = useMobileScreen();
+  const shouldNarrow =
+    !isMobileScreen && config.sidebarWidth < MIN_SIDEBAR_WIDTH;
 
-  使用效果(() => {
-    常数 酒吧宽度=应该缩小
-？窄边栏宽度
-      : 限制(配置。侧栏宽度 ?? 300);
-    常数 侧栏宽度= isMobileScreen？" 100瓦" : `${酒吧宽度}px ` 1;
-文档。documentElement.风格.setProperty("-侧栏宽度"，侧栏宽度);
-  }, [配置。侧栏宽度，是移动屏幕，应该缩小]);
+  useEffect(() => {
+    const barWidth = shouldNarrow
+      ? NARROW_SIDEBAR_WIDTH
+      : limit(config.sidebarWidth ?? 300);
+    const sideBarWidth = isMobileScreen ? "100vw" : `${barWidth}px`;
+    document.documentElement.style.setProperty("--sidebar-width", sideBarWidth);
+  }, [config.sidebarWidth, isMobileScreen, shouldNarrow]);
 
-  返回 {
-    翁德拉穆斯敦,
-    应该缩小,
+  return {
+    onDragMouseDown,
+    shouldNarrow,
   };
 }
 
-出口 功能 补充报道(小道具: { 类名？:字符串}) {
-  常数 聊天商店 = 使用聊天商店();
+export function SideBar(props: { className?: string }) {
+  const chatStore = useChatStore();
 
-  //拖动侧栏
-  常数 { 翁德拉穆斯敦, 应该缩小 } = 使用DragSideBar();
-  常数 航行 = 使用导航();
-  常数 配置 = useAppConfig();
+  // drag side bar
+  const { onDragMouseDown, shouldNarrow } = useDragSideBar();
+  const navigate = useNavigate();
+  const config = useAppConfig();
 
-  使用热键();
+  useHotKey();
 
-  返回 (
-    <差异
-      类名={`${风格。补充报道} ${道具。类名} ${
-应该缩小和样式["窄边栏"]
+  return (
+    <div
+      className={`${styles.sidebar} ${props.className} ${
+        shouldNarrow && styles["narrow-sidebar"]
       }`}
     >
-      <差异类名={风格["侧栏标题"]} 数据-Tauri-拖动-区域>
-        <差异类名={风格["侧栏标题"]} 数据-Tauri-拖动-区域>
-IOS-ChatGPT
-        </差异>
-        <差异类名={风格["侧栏-副标题"]}>
-        <a href=" http://yiqiwan.cc " 目标=" _blank ">个人主页</a>：公益密码-微信公众号:CydiaBi</差异><差异><a href=" https://idc.yiqiwan.cc " 目标=" _blank ">青云</a>：稳定安全的云服务器
-        </差异>
-        <差异类名={风格["侧栏-徽标"] + “无黑暗”}>
-          <ChatGptIcon/>
-        </差异>
-      </差异>
+      <div className={styles["sidebar-header"]} data-tauri-drag-region>
+        <div className={styles["sidebar-title"]} data-tauri-drag-region>
+          一起玩-ChatGPT
+        </div>
+        <div className={styles["sidebar-sub-title"]}>
+<a href="http://yiqiwan.cc" target="_blank">个人主页</a>：公益密码-微信公众号：CydiaBi</div><div><a href="https://idc.yiqiwan.cc" target="_blank">青云</a>：稳定安全的云服务器
+        </div>
+        <div className={styles["sidebar-logo"] + " no-dark"}>
+          <ChatGptIcon />
+        </div>
+      </div>
 
-      <差异类名={风格["侧边栏-标题栏"]}>
-        <图标按钮
-          图标={<马斯基孔/>}
-          文本={应该缩小？未定义:区域设置。面具.名字}
-          类名={风格["工具条-工具条-按钮"]}
-          onClick={() => 航行(路径。新聊天, { 状态: { 从家里: 真实的 } })}
-          阴影
+      <div className={styles["sidebar-header-bar"]}>
+        <IconButton
+          icon={<MaskIcon />}
+          text={shouldNarrow ? undefined : Locale.Mask.Name}
+          className={styles["sidebar-bar-button"]}
+          onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
+          shadow
         />
-        <图标按钮
-          图标={<PluginIcon/>}
-          文本={应该缩小？未定义:区域设置。插件.名字}
+        <IconButton
+          icon={<PluginIcon />}
+          text={shouldNarrow ? undefined : Locale.Plugin.Name}
           className={styles["sidebar-bar-button"]}
           onClick={() => showToast(Locale.WIP)}
           shadow
@@ -174,7 +173,7 @@ IOS-ChatGPT
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href={REPO_URL} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
@@ -199,9 +198,7 @@ IOS-ChatGPT
       <div
         className={styles["sidebar-drag"]}
         onMouseDown={(e) => onDragMouseDown(e as any)}
-      >
-        <DragIcon />
-      </div>
+      ></div>
     </div>
   );
 }
